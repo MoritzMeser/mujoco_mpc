e5f9466be63d1ffcfe3ca2046e28cdb07e375166
Moritz Meser <moritzmeser@web.de>
2024-05-04 20:12:22 +0200

    some more things

--- mjpc/tasks/H1/walk/H1_walk.cc
+++ mjpc/tasks/H1/walk/H1_walk.cc
@@ -34,6 +34,9 @@ namespace mjpc {
 // -------------------------------------------------------------
     void H1_walk::ResidualFn::Residual(const mjModel *model, const mjData *data,
                                        double *residual) const {
+        // initialize reward
+        double reward = 1.0;
+
         // get values from GUI
         double stand_height = parameters_[1];
         double move_speed = parameters_[0];
@@ -45,14 +48,15 @@ namespace mjpc {
         double x = SensorByName(model, data, "head_height")[2];
         double standing = tolerance(x, bounds, stand_height / 4);
 
+        reward *= standing;
+
 
         // ----- torso upright ----- //
         bounds = {0.9, infinity};
         x = SensorByName(model, data, "torso_upright")[2];
         double upright = tolerance(x, bounds, 1.9);
 
-        // ----- stand_reward ----- //
-        double stand_reward = standing * upright;
+        reward *= upright;
 
 
         // ----- small control ----- //
@@ -66,18 +70,37 @@ namespace mjpc {
         small_control /= model->nu;  // average over all controls
         small_control = (4 + small_control) / 5;
 
+        reward *= small_control;
+
 
         // ----- actuator velocity ----- //
-        double margin = parameters_[2];
-        double vel_bounds = parameters_[3];
+        double vel_margin = parameters_[2];
         double actuator_velocity = 0.0;
         for (int i = 0; i < model->nu; i++) {
-            double value_at_margin = 0.0;
+
             x = data->actuator_velocity[i];
-            actuator_velocity += tolerance(x, {-vel_bounds, value_at_margin}, margin, "quadratic", value_at_margin);
+//            x = data->cvel[i];
+
+            actuator_velocity += tolerance(x, {0, 0}, vel_margin, "quadratic", 0.0);
         }
         actuator_velocity /= model->nu;  // average over all controls
         actuator_velocity = (2 + actuator_velocity) / 3;
+
+        reward *= actuator_velocity;
+        // the moment seems to do nothing (most values are always zero anyway)
+
+//        // ----- actuator moment ----- //
+//        double moment_margin = parameters_[3];
+//        double actuator_moment = 0.0;
+//        for (int i = 0; i < model->nu; i++) {
+//            x = data->actuator_moment[i];
+//            printf("actuator_moment: %f\n", x);
+//            actuator_moment += tolerance(x, {0, 0}, moment_margin, "gaussian", 0.1);
+//        }
+//        actuator_moment /= model->nu;  // average over all controls
+//        actuator_moment = (2 + actuator_moment) / 3;
+//
+//        reward *= actuator_moment;
 
         // ----- foot height ----- //
         double right_foot_height = SensorByName(model, data, "right_foot_height")[2];
@@ -87,6 +110,7 @@ namespace mjpc {
         double foot_reward = (right_foot_reward + left_foot_reward) / 2;
         foot_reward = (4 + foot_reward) / 5;
 
+        reward *= foot_reward;
 
 //        // ----- foot distance ----- //
 //        double left_foot_x = SensorByName(model, data, "left_foot_height")[0];
@@ -101,19 +125,18 @@ namespace mjpc {
 //        foot_reward = (4 + foot_reward) / 5;
 
         // ----- reward computation ----- //
-        double reward;
         // ----- move speed ----- //
         if (move_speed == 0.0) {
             double horizontal_velocity_x = SensorByName(model, data, "center_of_mass_velocity")[0];
             double horizontal_velocity_y = SensorByName(model, data, "center_of_mass_velocity")[1];
             double dont_move = (tolerance(horizontal_velocity_x, {0.0, 0.0}, 2) +
                                 tolerance(horizontal_velocity_y, {0.0, 0.0}, 2)) / 2;
-            reward = small_control * stand_reward * dont_move * actuator_velocity * foot_reward;
+            reward *= dont_move;
         } else {
             double com_velocity = SensorByName(model, data, "center_of_mass_velocity")[0];
             double move = tolerance(com_velocity, {move_speed, infinity}, std::abs(move_speed), "linear", 0.0);
             move = (5 * move + 1) / 6;
-            reward = small_control * stand_reward * move * actuator_velocity * foot_reward;
+            reward *= move;
         }
 
         // ----- residuals ----- //
