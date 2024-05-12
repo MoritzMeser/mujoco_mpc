620a3044f39b7f6a4e8b55710339d73c174724d4
Moritz Meser <moritzmeser@web.de>
2024-05-12 10:08:08 +0200

    cleaned up package task

--- mjpc/tasks/H1/package/package.cc
+++ mjpc/tasks/H1/package/package.cc
@@ -16,6 +16,8 @@
 
 #include <string>
 # include <limits>
+#include <cmath>
+#include <algorithm>
 
 #include "mujoco/mujoco.h"
 #include "mjpc/utilities.h"
@@ -36,74 +38,29 @@ namespace mjpc {
                                           double *residual) const {
         // get values from GUI
         double stand_height = parameters_[1];
-//        double move_speed = parameters_[0];
 
         // ----- standing ----- //
-        double infinity = std::numeric_limits<double>::infinity();
-        std::pair<double, double> bounds = {stand_height, infinity};
-
-        double x = SensorByName(model, data, "head_height")[2];
-        double standing = tolerance(x, bounds, stand_height / 4);
+        double head_height = SensorByName(model, data, "head_height")[2];
+        double standing = tolerance(head_height, {stand_height, INFINITY}, stand_height / 4);
 
 
         // ----- torso upright ----- //
-        bounds = {0.9, infinity};
-        x = SensorByName(model, data, "torso_upright")[2];
-        double upright = tolerance(x, bounds, 1.9);
+        double torso_upright = SensorByName(model, data, "torso_upright")[2];
+        double upright = tolerance(torso_upright, {0.9, INFINITY}, 1.9);
 
-        // ----- stand_reward ----- //
         double stand_reward = standing * upright;
 
 
         // ----- small control ----- //
         double small_control = 0.0;
         for (int i = 0; i < model->nu; i++) {
-            double margin = 10;
-            double value_at_margin = 0.0;
-            x = data->ctrl[i];
-            small_control += tolerance(x, {0.0, 0.0}, margin, "quadratic", value_at_margin);
+            small_control += tolerance(data->ctrl[i], {0.0, 0.0}, 10.0, "quadratic", 0.0);
         }
         small_control /= model->nu;  // average over all controls
         small_control = (4 + small_control) / 5;
 
+        // ----- rewards specific to the package task ----- //
 
-        // ----- actuator velocity ----- //
-        double margin = parameters_[2];
-        double vel_bounds = parameters_[3];
-        double actuator_velocity = 0.0;
-        for (int i = 0; i < model->nu; i++) {
-            double value_at_margin = 0.0;
-            x = data->actuator_velocity[i];
-            actuator_velocity += tolerance(x, {-vel_bounds, value_at_margin}, margin, "quadratic", value_at_margin);
-        }
-        actuator_velocity /= model->nu;  // average over all controls
-        actuator_velocity = (2 + actuator_velocity) / 3;
-
-        // ----- foot height ----- //
-        double right_foot_height = SensorByName(model, data, "right_foot_height")[2];
-        double left_foot_height = SensorByName(model, data, "left_foot_height")[2];
-        double right_foot_reward = tolerance(right_foot_height, {0.0, 0.2}, 0.1);
-        double left_foot_reward = tolerance(left_foot_height, {0.0, 0.2}, 0.1);
-        double foot_reward = (right_foot_reward + left_foot_reward) / 2;
-        foot_reward = (4 + foot_reward) / 5;
-
-
-//        // ----- foot distance ----- //
-//        double left_foot_x = SensorByName(model, data, "left_foot_height")[0];
-//        double right_foot_x = SensorByName(model, data, "right_foot_height")[0];
-//        double left_foot_y = SensorByName(model, data, "left_foot_height")[1];
-//        double right_foot_y = SensorByName(model, data, "right_foot_height")[1];
-//        double foot_distance = std::sqrt(std::pow(left_foot_x - right_foot_x, 2) +
-//                                         std::pow(left_foot_y - right_foot_y, 2));
-//        foot_distance = tolerance(foot_distance, {0.2, 0.6}, 0.1);
-//        foot_reward *= foot_distance;
-//
-//        foot_reward = (4 + foot_reward) / 5;
-
-#include <cmath>
-#include <algorithm>
-
-// Assuming package_location, package_destination, right_hand_location, and left_hand_location are std::array<double, 3>
         double *package_location = SensorByName(model, data, "package_location");
         double *package_destination = SensorByName(model, data, "package_destination");
         double *left_hand_location = SensorByName(model, data, "left_hand_position");
@@ -126,7 +83,7 @@ namespace mjpc {
         bool reward_success = dist_package_destination < 0.1;
 
         // ----- reward computation ----- //
-        double reward = (
+        double const humanoid_bench_reward = (
                 stand_reward * small_control
                 - 3 * dist_package_destination
                 - (dist_hand_package_left + dist_hand_package_right) * 0.1
@@ -136,7 +93,9 @@ namespace mjpc {
 
 
         // ----- residuals ----- //
-        residual[0] = std::exp(-reward);
+
+        // because the reward in this task is not bound to [0, 1] we need to use the exponential function
+        residual[0] = std::exp(-humanoid_bench_reward);
     }
 
 // -------- Transition for H1 package task --------
