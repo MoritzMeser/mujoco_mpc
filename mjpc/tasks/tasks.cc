c283b00d42ed5c1c27896d14458c7c91a1d2cc95
Moritz Meser <moritzmeser@web.de>
2024-05-15 15:46:48 +0200

    able to load G1

--- mjpc/tasks/tasks.cc
+++ mjpc/tasks/tasks.cc
@@ -40,12 +40,14 @@
 #include "mjpc/tasks/walker/walker.h"
 #include "mjpc/tasks/H1/walk/H1_walk.h"
 #include "mjpc/tasks/H1/balance_simple/balance_simple.h"
+#include "mjpc/tasks/G1/walk/G1_walk.h"
 
 namespace mjpc {
 
     std::vector<std::shared_ptr<Task>> GetTasks() {
         return {
                 std::make_shared<H1_walk>(),
+                std::make_shared<G1_walk>(),
                 std::make_shared<Balance_Simple>(),
                 std::make_shared<Acrobot>(),
                 std::make_shared<Allegro>(),
