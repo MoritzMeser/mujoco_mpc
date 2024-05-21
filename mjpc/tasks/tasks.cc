26a71c3408d949a2f3db929e7e4406abb6acfd68
Moritz Meser <moritzmeser@web.de>
2024-05-21 20:29:10 +0200

    started kitchen, but not working

--- mjpc/tasks/tasks.cc
+++ mjpc/tasks/tasks.cc
@@ -59,6 +59,7 @@
 #include "mjpc/tasks/Humanoid_Bench_H1/basketball/H1_basketball.h"
 #include "mjpc/tasks/Humanoid_Bench_H1/window/H1_window.h"
 #include "mjpc/tasks/Humanoid_Bench_H1/spoon/H1_spoon.h"
+#include "mjpc/tasks/Humanoid_Bench_H1/kitchen/H1_kitchen.h"
 
 
 #include "mjpc/tasks/Humanoid_Bench_H1/balance_simple/balance_simple.h"
@@ -90,6 +91,7 @@ namespace mjpc {
                 std::make_shared<H1_basketball>(),
                 std::make_shared<H1_window>(),
                 std::make_shared<H1_spoon>(),
+//                std::make_shared<H1_kitchen>(), //TODO: Fix H1_kitchen, currently not working
 
 //                std::make_shared<Balance_Simple>(),
 //                std::make_shared<H1_package>(),
