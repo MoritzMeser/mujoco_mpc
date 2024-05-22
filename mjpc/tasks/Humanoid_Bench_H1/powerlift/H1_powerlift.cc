f2627c1c926ea557e09ce3c887bfe87e21054e14
Moritz Meser <moritzmeser@web.de>
2024-05-22 16:10:53 +0200

    implemented reward for powerlift

--- mjpc/tasks/Humanoid_Bench_H1/powerlift/H1_powerlift.cc
+++ mjpc/tasks/Humanoid_Bench_H1/powerlift/H1_powerlift.cc
@@ -24,13 +24,30 @@ namespace mjpc {
 
 // -------------------------------------------------------------
     void H1_powerlift::ResidualFn::Residual(const mjModel *model, const mjData *data,
-                                       double *residual) const {
-        //TODO: implement this
-        double reward = 1.0;
+                                            double *residual) const {
+        double const standHeight = 1.65;
+
+        double standing = tolerance(SensorByName(model, data, "head_height")[2], {standHeight, INFINITY},
+                                    standHeight / 3);
+
+        double upright = tolerance(SensorByName(model, data, "torso_upright")[2], {0.8, INFINITY}, 1.9, "linear", 0.0);
+
+        double stand_reward = standing * upright;
+
+        double small_control = 0.0;
+        for (int i = 0; i < model->nu; i++) {
+            small_control += tolerance(data->ctrl[i], {0.0, 0.0}, 10.0, "quadratic", 0.0);
+        }
+        small_control /= model->nu;  // average over all controls
+        small_control = (4 + small_control) / 5;
+
+        double dumbbell_height = SensorByName(model, data, "dumbbell_height")[2];
+        double reward_dumbbell_lifted = tolerance(dumbbell_height, {1.9, 2.1}, 2.0);
+
+        double reward = 0.2 * (small_control * stand_reward) + 0.8 * reward_dumbbell_lifted;
 
         // ----- residuals ----- //
-
-        residual[0] = std::exp(-reward);
+        residual[0] = 1.0 - reward;
     }
 
 // -------- Transition for Humanoid_Bench_H1 powerlift task -------- //
