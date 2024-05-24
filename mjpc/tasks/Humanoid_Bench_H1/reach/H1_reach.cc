109e91b37dd047b4849a10599fe8704d09f50cd1
Moritz Meser <moritzmeser@web.de>
2024-05-24 12:33:22 +0200

    implemented reach task

--- mjpc/tasks/Humanoid_Bench_H1/reach/H1_reach.cc
+++ mjpc/tasks/Humanoid_Bench_H1/reach/H1_reach.cc
@@ -22,6 +22,8 @@
 #include "mjpc/utilities.h"
 
 #include "mjpc/utility/dm_control_utils_rewards.h"
+#include <array>
+#include <random>
 
 namespace mjpc {
     std::string H1_reach::XmlPath() const {
@@ -48,14 +50,36 @@ namespace mjpc {
         double reward_close = (hand_dist < 1) ? 5 : 0;
         double reward_success = (hand_dist < 0.05) ? 10 : 0;
 
+        // ----- reward ----- //
         double reward = healthy_reward - 0.0001 * motion_penalty + reward_close + reward_success;
-        residual[0] = 20 - reward;  // 20 is the maximum reward --> change this if the reward-computation changes
+
+        // ----- residual ----- //
+        residual[0] = std::exp(-reward);
     }
 
 // -------- Transition for Humanoid_Bench_H1 reach task -------- //
 // ------------------------------------------------------------- //
     void H1_reach::TransitionLocked(mjModel *model, mjData *data) {
-        //
+        double *goal_pos = SensorByName(model, data, "goal_pos");
+        double *left_hand_pos = SensorByName(model, data, "left_hand_position");
+        double hand_dist = std::sqrt(std::pow(goal_pos[0] - left_hand_pos[0], 2) +
+                                     std::pow(goal_pos[1] - left_hand_pos[1], 2) +
+                                     std::pow(goal_pos[2] - left_hand_pos[2], 2));
+        // check if task is done
+        if (hand_dist < 0.1) {
+            // generate new random target
+            std::array<double, 3> target_low = {-2, -2, 0.2};
+            std::array<double, 3> target_high = {2, 2, 2.0};
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::array<double, 3> new_target = {0, 0, 0};
+            for (int i = 0; i < 3; ++i) {
+                std::uniform_real_distribution<> dis(target_low[i], target_high[i]);
+                new_target[i] = dis(gen);
+            }
+            // copy new target to mocap_pos
+            mju_copy3(data->mocap_pos, new_target.data());
+        }
     }
 
 }  // namespace mjpc
