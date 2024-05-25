d447b5d4c33a19069a1c6cfc36c3ed873c41d4b0
Moritz Meser <moritzmeser@web.de>
2024-05-25 18:43:52 +0200

    implemented reward truck task

--- mjpc/tasks/Humanoid_Bench_H1/truck/H1_truck.h
+++ mjpc/tasks/Humanoid_Bench_H1/truck/H1_truck.h
@@ -4,6 +4,7 @@
 
 #ifndef MUJOCO_MPC_H1_TRUCK_H
 #define MUJOCO_MPC_H1_TRUCK_H
+
 #include <string>
 #include "mujoco/mujoco.h"
 #include "mjpc/task.h"
@@ -12,19 +13,29 @@
 namespace mjpc {
     class H1_truck : public Task {
     public:
+        bool IsPackageUponTable(const mjModel *model, const mjData *data, const std::string &package) const;
+
         std::string Name() const override;
 
         std::string XmlPath() const override;
 
         class ResidualFn : public mjpc::BaseResidualFn {
         public:
-            explicit ResidualFn(const H1_truck *task) : mjpc::BaseResidualFn(task) {}
+            explicit ResidualFn(const H1_truck *task) : mjpc::BaseResidualFn(task),
+                                                        task_(const_cast<H1_truck *>(task)) {}
 
             void Residual(const mjModel *model, const mjData *data,
                           double *residual) const override;
+
+        private:
+            H1_truck *task_;
         };
 
-        H1_truck() : residual_(this) {}
+        H1_truck() : residual_(this),
+                     initial_zs_({}),
+                     packages_on_truck_({"package_a", "package_b", "package_c", "package_d", "package_e"}),
+                     packages_picked_up_({}),
+                     packages_upon_table_({}) {}
 
 // -------- Transition for Humanoid_Bench_H1 truck task -------- //
 // ------------------------------------------------------------ //
@@ -39,6 +50,10 @@ namespace mjpc {
 
     private:
         ResidualFn residual_;
+        std::vector<double> initial_zs_;
+        std::vector<std::string> packages_on_truck_;
+        std::vector<std::string> packages_picked_up_;
+        std::vector<std::string> packages_upon_table_;
     };
 }  // namespace mjpc
 
