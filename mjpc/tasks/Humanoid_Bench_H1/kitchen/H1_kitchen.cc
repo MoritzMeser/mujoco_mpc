c9ef42c36db2bb89190a4fd6584df8cc8d0856ec
Moritz Meser <moritzmeser@web.de>
2024-05-27 21:39:19 +0200

    started to implement reward kitchen task - not finished

--- mjpc/tasks/Humanoid_Bench_H1/kitchen/H1_kitchen.cc
+++ mjpc/tasks/Humanoid_Bench_H1/kitchen/H1_kitchen.cc
@@ -24,19 +24,90 @@ namespace mjpc {
 
 // -------------------------------------------------------------
     void H1_kitchen::ResidualFn::Residual(const mjModel *model, const mjData *data,
-                                       double *residual) const {
-        //TODO: implement this
-        double reward = 1.0;
+                                          double *residual) const {
+        // the reward of this task is a count of how many objects are at the target location
+
+        double const BONUS_THRESH = 0.3;
+        // ----- initialize reward ----- //
+        int reward = 0;
+
+        // initialize target positions
+        std::map<std::string, std::pair<double, double>> target_positions = {
+                {"bottom burner", {-0.88, -0.01}},
+                {"top burner",    {-0.92, -0.01}},
+                {"light switch",  {-0.69, -0.05}},
+                {"slide cabinet", {0.37,  0.0}},
+                {"hinge cabinet", {0.0,   1.45}},
+                {"microwave",     {-0.75, 0.0}},
+                {"kettle",        {-0.23, 0.75}}
+        };
+
+        bool all_completed_so_far = true;
+        // ----- loop over all tasks ----- //
+        for (const std::string &task: task_->tasks_to_complete_) {
+            // ----- get the position of the object ----- //
+            double *object_pos = SensorByName(model, data, task);
+            // ----- get the position of the target ----- //
+            std::pair target_location = target_positions[task];
+            // ----- calculate the distance between the object and the target ----- //
+            double distance = std::sqrt(std::pow(object_pos[0] - target_location.first, 2) +
+                                        std::pow(object_pos[1] - target_location.second, 2));
+            // ----- check if the object is at the target location ----- //
+            bool completed = distance < BONUS_THRESH;
+            if (completed && (all_completed_so_far || !task_->ENFORCE_TASK_ORDER)) {
+                reward++;
+                all_completed_so_far = all_completed_so_far && completed;
+            }
+        }
 
         // ----- residuals ----- //
-
         residual[0] = std::exp(-reward);
     }
 
 // -------- Transition for Humanoid_Bench_H1 kitchen task -------- //
 // ------------------------------------------------------------ //
     void H1_kitchen::TransitionLocked(mjModel *model, mjData *data) {
-        //
+        if (!REMOVE_TASKS_WHEN_COMPLETE) {
+            return;
+        }
+        double const BONUS_THRESH = 0.3;
+
+        // initialize target positions
+        std::map<std::string, std::pair<double, double>> target_positions = {
+                {"bottom burner", {-0.88, -0.01}},
+                {"top burner",    {-0.92, -0.01}},
+                {"light switch",  {-0.69, -0.05}},
+                {"slide cabinet", {0.37,  0.0}},
+                {"hinge cabinet", {0.0,   1.45}},
+                {"microwave",     {-0.75, 0.0}},
+                {"kettle",        {-0.23, 0.75}}
+        };
+
+        bool all_completed_so_far = true;
+        std::vector<std::string> completed_tasks;
+        // ----- loop over all tasks ----- //
+        for (const std::string &task: tasks_to_complete_) {
+            // ----- get the position of the object ----- //
+            double *object_pos = SensorByName(model, data, task);
+            // ----- get the position of the target ----- //
+            std::pair target_location = target_positions[task];
+            // ----- calculate the distance between the object and the target ----- //
+            double distance = std::sqrt(std::pow(object_pos[0] - target_location.first, 2) +
+                                        std::pow(object_pos[1] - target_location.second, 2));
+            // ----- check if the object is at the target location ----- //
+            bool completed = distance < BONUS_THRESH;
+            if (completed && (all_completed_so_far || !ENFORCE_TASK_ORDER)) {
+                completed_tasks.push_back(task);
+                all_completed_so_far = all_completed_so_far && completed;
+            }
+            // remove all completed tasks
+            for (const std::string &completedTask: completed_tasks) {
+                tasks_to_complete_.erase(
+                        std::remove(tasks_to_complete_.begin(), tasks_to_complete_.end(), completedTask),
+                        tasks_to_complete_.end());
+            }
+        }
+
     }
 
 }  // namespace mjpc
--- mjpc/tasks/Humanoid_Bench_H1/kitchen/H1_kitchen.h
+++ mjpc/tasks/Humanoid_Bench_H1/kitchen/H1_kitchen.h
@@ -4,6 +4,7 @@
 
 #ifndef MUJOCO_MPC_H1_KITCHEN_H
 #define MUJOCO_MPC_H1_KITCHEN_H
+
 #include <string>
 #include "mujoco/mujoco.h"
 #include "mjpc/task.h"
@@ -18,13 +19,21 @@ namespace mjpc {
 
         class ResidualFn : public mjpc::BaseResidualFn {
         public:
-            explicit ResidualFn(const H1_kitchen *task) : mjpc::BaseResidualFn(task) {}
+            explicit ResidualFn(const H1_kitchen *task) : mjpc::BaseResidualFn(task),
+                                                          task_(const_cast<H1_kitchen *>(task)) {}
 
             void Residual(const mjModel *model, const mjData *data,
                           double *residual) const override;
+
+        private:
+            H1_kitchen *task_;
         };
 
-        H1_kitchen() : residual_(this) {}
+        H1_kitchen() : residual_(this),
+                       REMOVE_TASKS_WHEN_COMPLETE(true),
+                       TERMINATE_ON_TASK_COMPLETE(true),
+                       ENFORCE_TASK_ORDER(true),
+                       tasks_to_complete_({"microwave", "kettle", "bottom burner", "light switch"}) {}
 
 // -------- Transition for Humanoid_Bench_H1 kitchen task -------- //
 // ------------------------------------------------------------ //
@@ -39,6 +48,10 @@ namespace mjpc {
 
     private:
         ResidualFn residual_;
+        bool REMOVE_TASKS_WHEN_COMPLETE;
+        bool TERMINATE_ON_TASK_COMPLETE;
+        bool ENFORCE_TASK_ORDER;
+        std::vector<std::string> tasks_to_complete_;
     };
 }  // namespace mjpc
 
