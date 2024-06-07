3ca18170b1d912f0135de458a1b446dfbed6d2d0
Moritz Meser <moritzmeser@web.de>
2024-06-07 11:01:35 +0200

    slider for individual reward componentes - stand task only for now

--- mjpc/tasks/humanoid_bench/basic_locomotion/stand/H1_stand.cc
+++ mjpc/tasks/humanoid_bench/basic_locomotion/stand/H1_stand.cc
@@ -10,6 +10,7 @@
 #include "mjpc/utilities.h"
 
 #include "mjpc/tasks/humanoid_bench/basic_locomotion/walk_reward.h"
+#include "mjpc/tasks/humanoid_bench/utility/dm_control_utils_rewards.h"
 
 
 namespace mjpc {
@@ -19,8 +20,52 @@ namespace mjpc {
         double const walk_speed = 0.0;
         double const stand_height = 1.65;
 
+        int residual_counter = 0;
+        // ----- original humanoid bench reward ----- //
         double reward = walk_reward(model, data, walk_speed, stand_height);
-        residual[0] = std::exp(-reward);
+        residual[residual_counter++] = std::exp(-reward);
 
+        // ----- individual reward terms ----- //
+
+        // ----- standing ----- //
+        double head_height = SensorByName(model, data, "head_height")[2];
+        double standing = tolerance(head_height, {stand_height, INFINITY}, stand_height / 4);
+
+        residual[residual_counter++] = 1.0 - standing;
+
+
+        // ----- torso upright ----- //
+        double torso_upright = SensorByName(model, data, "torso_upright")[2];
+        double upright = tolerance(torso_upright, {0.9, INFINITY}, 1.9);
+
+        residual[residual_counter++] = 1.0 - upright;
+
+
+        // ----- small control ----- //
+        double small_control = 0.0;
+        for (int i = 0; i < model->nu; i++) {
+            small_control += tolerance(data->ctrl[i], {0.0, 0.0}, 10.0, "quadratic", 0.0);
+        }
+        small_control /= model->nu;  // average over all controls
+
+        residual[residual_counter++] = 1.0 - small_control;
+
+
+        // ----- move speed ----- //
+        double move_reward = 1.0;
+        if (walk_speed == 0.0) {
+            double horizontal_velocity_x = SensorByName(model, data, "center_of_mass_velocity")[0];
+            double horizontal_velocity_y = SensorByName(model, data, "center_of_mass_velocity")[1];
+            double dont_move = (tolerance(horizontal_velocity_x, {0.0, 0.0}, 2) +
+                                tolerance(horizontal_velocity_y, {0.0, 0.0}, 2)) / 2;
+            move_reward = dont_move;
+        } else {
+            double com_velocity = SensorByName(model, data, "center_of_mass_velocity")[0];
+            double move = tolerance(com_velocity, {walk_speed, INFINITY}, std::abs(walk_speed), "linear", 0.0);
+            move = (5 * move + 1) / 6;
+            move_reward = move;
+        }
+
+        residual[residual_counter++] = 1.0 - move_reward;
     }
 }  // namespace mjpc
